b2d1e68cebc84aa34a5b941131eb20d575eb1e32
Daniel Evani <danielevani97@gmail.com>
2021-05-11 19:24:43 -0300

    Controllers createds and routes generateds with scope /api and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Aqui estamos definindo que todas as urls devem ser precedidas por /api
+  # Ex: localhost:3000/api/restaurants
+  scope '/api', defaults: { format: :json } do
+    # Quando utilizamos o resources, sao criadas as urls no formato REST
+    # o rest possui urls de criar, listar, atualizar, deletar
+    resources :categories, only: [:index]
+    resources :restaurants, only: [:index, :show]
+    resources :orders, only: [:create, :show]
+    resources :available_cities, only: [:index]
+  end
 end
