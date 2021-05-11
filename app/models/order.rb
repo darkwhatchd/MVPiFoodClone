983a64451f84700b798006688506634b450086ad
Daniel Evani <danielevani97@gmail.com>
2021-05-11 18:56:18 -0300

    Validations addeds to models, enum used to manage status of Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :restaurant
   has_many :order_products
+
+  validates :name, :phone_number, :total_value, :city, :neighborhood, :street, :number, presence: true
+
+  # Enum serve para que possamos utilizar os valores predefinidos como se fossem os inteiros:
+  # Nesse caso, waiting == 0 e delivered == 1
+  enum status: { waiting: 0, delivered: 1 }
 end
