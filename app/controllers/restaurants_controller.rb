5e38a464131e197555340a4d306b9b08cec87155
Daniel Evani <danielevani97@gmail.com>
2021-05-15 16:06:24 -0300

    Create filters to search restaurants. Filters by query, cities, category.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,9 @@ class RestaurantsController < ApplicationController
   # Pegamos todos os restaurantes do Banco de Dados
   def index
     @restaurants = Restaurant.all
+    filter_by_query if params[:q]
+    filter_by_city if params[:city]
+    filter_by_category if params[:category]
   end
 
   def show
@@ -14,5 +17,22 @@ class RestaurantsController < ApplicationController
   # essa variável será utilizada no jbuilder
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Utilizamos a gem ransack para ajudar no filtro por query
+  # A gem pega o resultado do parametro "q", verificando se esse parametro está dentro do nome
+  # ou descrição de algum restaurante.
+  def filter_by_query
+    @restaurants = @restaurants.ransack(name_or_description_cont: params[:q]).result
+  end
+
+  def filter_by_city
+    @restaurants = @restaurants.where(city: params[:city])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
   end
 end
