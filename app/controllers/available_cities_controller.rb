2822c97d5ebcd9eced85f681c19b8206c126487d
Daniel Evani <danielevani97@gmail.com>
2021-05-15 15:33:33 -0300

    Available Cities controller index map all cities disponibles without repeat

--- app/controllers/available_cities_controller.rb
+++ app/controllers/available_cities_controller.rb
@@ -1,4 +1,7 @@
 class AvailableCitiesController < ApplicationController
+  # Aqui fazemos um mapeamento em todos os restaurantes, pegando as cidades deles,
+  # e utilizamos a função uniq, para evitar cidades repetidas
   def index
+    @available_cities = Restaurant.all.map { |r| r.city }.uniq
   end
 end
